fbc338ac68a88d4488e432abd7c3334856be2532
simonetome <simone.tome@polimi.it>
2026-04-16 10:52:19 +0200

    Add embeddings

--- scripts/1_get_data.sh
+++ scripts/1_get_data.sh
@@ -9,3 +9,9 @@ if [ ! -e data/efo_otar_slim.owl ]; then
     echo "Downloading EFO owl"
     wget https://github.com/EBISPOT/efo/releases/download/v3.88.0/efo_otar_slim.owl -P data
 fi
+
+
+if [ ! -e data/disease ]; then
+    echo "Downloading disease info from OT release March 2026"
+    rsync -rpltvz --delete rsync.ebi.ac.uk::pub/databases/opentargets/platform/26.03/output/disease data/.
+fi
