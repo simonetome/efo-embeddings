b8987d44db1ef68eb97fc9d6ed0893226ce2ae42
simonetome <simone.tome@polimi.it>
2026-05-12 14:41:43 +0200

    Adjusting dependencies

--- scripts/1_get_data.sh
+++ scripts/1_get_data.sh
@@ -4,12 +4,10 @@ mkdir -p data
 mkdir -p embeddings
 mkdir -p cache
 
-
 if [ ! -e data/efo_otar_slim.owl ]; then
     echo "Downloading EFO owl"
     wget https://github.com/EBISPOT/efo/releases/download/v3.88.0/efo_otar_slim.owl -P data
 fi
-
 
 if [ ! -e data/disease ]; then
     echo "Downloading disease info from OT release March 2026"
